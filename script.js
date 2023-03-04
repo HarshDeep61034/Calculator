5605ab63225f98d4f4b52a706edbe48b41cc4a80
HarshDeep61034 <hdeep61034@gmail.com>
2023-03-04 15:34:09 +0530

    made the keys and clear button work

--- script.js
+++ script.js
@@ -30,8 +30,18 @@ function operator(x, a, b){
     }
 }
 
+// Accessing  DOM Elements
 const displayEl = document.getElementsByClassName('display')[0];
+const clearEl = document.getElementById('clear');
 
-function clear(){
-    displayEl.innerHTML = "h";
+// clearing Display Function
+clearEl.addEventListener('click', ()=>{
+    displayEl.textContent = "0";
+})
+
+// Display function
+var value = "";
+function display(clicked_id){
+    value = value+clicked_id;
+    displayEl.textContent = value;
 }
